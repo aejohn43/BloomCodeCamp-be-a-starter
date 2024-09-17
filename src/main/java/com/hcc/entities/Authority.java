c1aa9c83a915fdcd3a33a7a6b37dbf1842e7f783
Enock Akpaki <aenockjohn@gmail.com>
2024-09-17 03:24:07 -0700

    User and Assignment Entities added, Security Config file created, Authority Entity added and User entity update to implement user details

---
+++ src/main/java/com/hcc/entities/Authority.java
@@ -0,0 +1,41 @@
+package com.hcc.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "authorities")
+public class Authority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "authority")
+    private String authority;
+    @Column(name = "user")
+    @ManyToOne
+    @JoinColumn(name = "")
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
