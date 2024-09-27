0c19b335eef10ada2b9a3a65e288af338ef8a4e7
Enock Akpaki <aenockjohn@gmail.com>
2024-09-27 14:56:31 -0700

    Modifications added by Swastik to the Assignement entitie

--- src/main/java/com/hcc/config/SecurityConfig.java
+++ src/main/java/com/hcc/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.hcc.config;
 
-import com.hcc.filters.jwtFilter;
+import com.hcc.filters.JwtFilter;
 import com.hcc.services.UserDetailServiceImpl;
 import com.hcc.utils.CustomPasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +25,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
     CustomPasswordEncoder customPasswordEncoder;
 
     @Autowired
-    jwtFilter jwtFilt;
+    JwtFilter jwtFilt;
 
     @Override @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
@@ -54,4 +54,4 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
         http.addFilterBefore(jwtFilt, UsernamePasswordAuthenticationFilter.class);
     }
 
-}   
+}
--- src/main/java/com/hcc/entities/Assignment.java
+++ src/main/java/com/hcc/entities/Assignment.java
@@ -8,18 +8,42 @@ public class Assignment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "status")
     private String status;
+
     @Column(name = "number")
     private Integer number;
-    @Column(name = "githubUrl")
+
+    @Column(name = "github_url")
     private String githubUrl;
+
     @Column(name = "branch")
     private String branch;
-    @Column(name = "reviewVideoUrl")
-    private String reviewVideoUrl;
-    @JoinColumn(name = "user_id")
+
+    @Column(name = "code_review_video_url")
+    private String codeReviewVideoUrl;
+
+    @ManyToOne(optional = false)
     private User user;
+
+    @ManyToOne
+    private User codeReviewer;
+
+
+    // constructors
+    public Assignment() { }
+
+    public Assignment(String status, Integer number, String githubUrl, String branch, String codeReviewVideoUrl, User user) {
+        this.status = status;
+        this.number = number;
+        this.githubUrl = githubUrl;
+        this.branch = branch;
+        this.codeReviewVideoUrl = codeReviewVideoUrl;
+        this.user = user;
+    }
+
+    // getters and setters
 
     public Long getId() {
         return id;
@@ -41,7 +65,7 @@ public class Assignment {
         return number;
     }
 
-    public void setNumber(Integer number) {
+    public void setUmber(Integer number) {
         this.number = number;
     }
 
@@ -61,12 +85,16 @@ public class Assignment {
         this.branch = branch;
     }
 
-    public String getReviewVideoUrl() {
-        return reviewVideoUrl;
+    public String getCodeReviewVideoUrl() {
+        return codeReviewVideoUrl;
     }
 
-    public void setReviewVideoUrl(String reviewVideoUrl) {
-        this.reviewVideoUrl = reviewVideoUrl;
+    public void setCodeReviewVideoUrl(String codeReviewVideoUrl) {
+        this.codeReviewVideoUrl = codeReviewVideoUrl;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
     }
 
     public User getUser() {
@@ -75,5 +103,13 @@ public class Assignment {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public User getCodeReviewer() {
+        return codeReviewer;
+    }
+
+    public void setCodeReviewer(User codeReviewer) {
+        this.codeReviewer = codeReviewer;
     }
 }
